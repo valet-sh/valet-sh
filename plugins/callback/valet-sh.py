aa36c11a938f802a4e9fc9ea0c92a7465178217b
Johann Zelger <zelger@me.com>
2018-11-26 00:28:06 +0100

    major refactoring, abstract install playbook, elasticsearch role, ansible callback plugin, option parsing in cli

--- plugins/callback/valet-sh.py
+++ plugins/callback/valet-sh.py
@@ -1,7 +1,10 @@
-# (c) 2018 TechDivision GmbH
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
 import os
+import json
+import ast
 from ansible.plugins.callback.debug import CallbackModule as CallbackModule_debug
 
 class CallbackModule(CallbackModule_debug):
@@ -15,22 +18,42 @@ class CallbackModule(CallbackModule_debug):
     CALLBACK_TYPE = 'stdout'
     CALLBACK_NAME = 'valet-sh'
 
-    def __init__(self):
+    def v2_playbook_on_play_start(self, play):
         # create .inprogress flag file for valet.sh cli spinner to start
         open('/tmp/valet-sh.inprogress', 'a').close()
-        # call parent class function
-        super(CallbackModule, self).__init__()
+        name = play.get_name().strip()
+        self._play = play
+        #self._display.banner(msg)
+
+    def _print_task_banner(self, task):
+        return True
+
+    def v2_runner_on_skipped(self, result):
+        return True
+
+    def v2_runner_on_ok(self, result):
+        return True        
 
     def v2_playbook_on_stats(self, task):
         # remove .inprogress flag file for valet.sh cli spinner to stop
         os.unlink('/tmp/valet-sh.inprogress')
         # call parent class function
-        super(CallbackModule, self).v2_playbook_on_stats(task)
+        #super(CallbackModule, self).v2_playbook_on_stats(task)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
+        # get result ansible dict
+        resultDict = "%s" % (result._result)
 
-        # todo: parse result, reformat it and output to parent stdout
-        # self._display.display("%s" % (result._result))
+        # convert to json object
+        jsonObj = json.loads(json.dumps(ast.literal_eval(resultDict)))
+        # get msg from object
+        if 'msg' in jsonObj :
+            msg = jsonObj['msg']
+        if 'message' in jsonObj :
+            msg = jsonObj['message']
+
+        # send msg to stderr
+        self._display.display(msg, None, True)
 
         # call parent class function
         super(CallbackModule, self).v2_runner_on_failed(result, ignore_errors)
