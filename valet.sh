f85af3e7d16c669950c67f456c80c1492c002a33
Johann Zelger <j.zelger@techdivision.com>
2018-12-01 01:28:47 +0100

    outsourced (un)install functionality to its own repository, more refactoring

--- valet.sh
+++ valet.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-#
+
+################################################################################
 # valet.sh
 #
 # Copyright: (C) 2018 TechDivision GmbH - All Rights Reserved
 # Author: Johann Zelger <j.zelger@techdivision.com>
 # Author: Florian Schmid <f.schmid@techdivision.com>
-##
+################################################################################
 
 # Set a global trap for e.g. ctrl+c to run shutdown routine
 trap shutdown INT
@@ -15,8 +16,6 @@ APPLICATION_START_TIME=$(ruby -e 'puts Time.now.to_f');
 
 # define application to be auto startet in case of testing purpose for example
 : "${APPLICATION_AUTOSTART:=1}"
-# define default application mode for future usage
-: "${APPLICATION_MODE:=production}"
 # define default spinner enabled
 : "${SPINNER_ENABLED:=1}"
 # define application return codes
@@ -26,9 +25,6 @@ APPLICATION_RETURN_CODE_SUCCESS=0
 APPLICATION_RETURN_CODE=$APPLICATION_RETURN_CODE_SUCCESS
 # define default setting for debug info output
 APPLICATION_DEBUG_INFO_ENABLED=0;
-# define user export path enhancement
-# shellcheck disable=SC2016
-APPLICATION_EXPORT_PATH='export PATH="$PATH:$HOME/.valet-sh"'
 # define variables
 APPLICATION_NAME="valet.sh"
 # define default git relevant variables
@@ -37,14 +33,12 @@ APPLICATION_GIT_REPOSITORY=${APPLICATION_GIT_REPOSITORY:="valet-sh"}
 APPLICATION_GIT_URL=${APPLICATION_GIT_URL:="https://github.com/${APPLICATION_GIT_NAMESPACE}/${APPLICATION_GIT_REPOSITORY}"}
 # define default playbook dir
 ANSIBLE_PLAYBOOKS_DIR="playbooks"
-# define semver regular expression for checkout valid versions on upgrade
-SEMVER_REGEX="^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
 # define global temp dir
 TEMP_DIR="/tmp"
 # temporary application-in-progress file used e.g. by ansible or spinner function
 APPLICATION_INPROGRESS_FILE_PATH="${TEMP_DIR}/valet-sh.inprogress"
 # define default install directory
-INSTALL_DIR="$HOME/.${APPLICATION_GIT_REPOSITORY}";
+APPLICATION_REPO_DIR="/usr/local/${APPLICATION_GIT_NAMESPACE}/${APPLICATION_GIT_REPOSITORY}";
 # resolve symlinked bash source if needed
 test -h "${BASH_SOURCE[0]}" && SCRIPT_PATH="$(readlink "${BASH_SOURCE[0]}")" || SCRIPT_PATH="${BASH_SOURCE[0]}"
 # use current bash source script dir as base_dir
@@ -54,10 +48,8 @@ LOG_PATH=${BASE_DIR}/log
 
 # check if git dir is available in base dir
 if [ -d "${BASE_DIR}/.git" ]; then
-    # get the current version from git
+    # get the current version from git repository in base dir
     APPLICATION_VERSION=$(git --git-dir="${BASE_DIR}/.git" --work-tree="${BASE_DIR}" describe --tags)
-    # set cwd to base dir
-    cd "${BASE_DIR}" || error "Unable to set cwd to ${BASE_DIR}"
 fi
 
 ##############################################################################
@@ -129,27 +121,6 @@ function out() {
 }
 
 ##############################################################################
-# Validates version against semver
-##############################################################################
-function version_validate() {
-    local version=$1
-    if [[ "$version" =~ $SEMVER_REGEX ]]; then
-        if [ "$#" -eq "2" ]; then
-            local major=${BASH_REMATCH[1]}
-            local minor=${BASH_REMATCH[2]}
-            local patch=${BASH_REMATCH[3]}
-            local prere=${BASH_REMATCH[4]}
-            local build=${BASH_REMATCH[5]}
-            eval "$2=(\"$major\" \"$minor\" \"$patch\" \"$prere\" \"$build\")"
-            return 0
-        else
-            return 0
-        fi
-    fi
-    return 1
-}
-
-##############################################################################
 # Compares versions
 ##############################################################################
 function version_compare() {
@@ -181,123 +152,11 @@ function version_compare() {
 }
 
 ##############################################################################
-# Returns if application is installed
-##############################################################################
-function is_installed() {
-    if [ -d "${INSTALL_DIR}" ]
-    then
-        return 0
-    else
-        return 1
-    fi
-}
-
-##############################################################################
 # Prepares application by installing dependencies and itself
 ##############################################################################
 function prepare() {
-    install_deps
-    # install application if not done yet
-    if ! is_installed; then
-        selfinstall
-        # shutdown after initial installation
-        shutdown $APPLICATION_RETURN_CODE_SUCCESS
-    fi
     # set cwd to base dir
     cd "${BASE_DIR}" || error "Unable to set cwd to ${BASE_DIR}"
-}
-
-##############################################################################
-# Install ansible if not availabe
-##############################################################################
-function install_deps() {
-    # check if macOS command line tools are available by checking git bin
-    if [ ! -f /Library/Developer/CommandLineTools/usr/bin/git ]; then
-        # trigger git, this will prompt installation of Command Line tools
-        git &> /dev/null
-        # exit with error message, CLT should be installed by MacOS routine using the user prompt - this ensures clean install (without using a custom install routine)
-        error "Please install Command Line tools first (using the newly opened propmt) and re-run $APPLICATION_NAME"
-    fi
-
-    # check if ansible command is available
-    if [ ! -x "$(command -v ansible)" ]; then
-        # test and trigger sudo for MacOS timeout (sudo)
-        sudo true || error "Failed to sudo"
-
-        spinner_toggle "Installing Ansible" -f
-        # if ansible is not available, install pip and ansible
-        # Todo: Redirect output to logfile
-        sudo easy_install pip &> /dev/null || error "Failed to installed pip"
-        sudo pip install -Iq ansible &> /dev/null || error "Failed to install ansible"
-        spinner_toggle
-    fi
-
-    # verfiy all dependencies
-    verify_deps
-}
-
-
-##############################################################################
-# Verify dependencies
-##############################################################################
-function verify_deps() {
-    # check if ansible command is available
-    if [ ! -x "$(command -v ansible-playbook)" ]; then
-        error "Dependency not met: Failed to run ansible-playbook command"
-    fi
-}
-
-##############################################################################
-# Uninstall application
-##############################################################################
-function uninstall() {
-    # TODO: call ansible playbook to uninstall all installed packages 
-    #       and remove services first
-    # TODO: remove PATH enhancements in zshrc and bash_profile dotfiles
-    spinner_toggle "Uninstalling" -f
-    # remove application itselfe
-    rm -rf "${INSTALL_DIR}" &> /dev/null
-    spinner_toggle
-    out success "Successfully uninstalled ${APPLICATION_NAME}. Good bye :)"
-    shutdown
-}
-
-##############################################################################
-# Install meachanism
-##############################################################################
-function selfinstall() {
-    # check if not installed
-    if ! is_installed;
-    then
-        spinner_toggle "Installing" -f
-        # remove old install directory (< version 1.0.0-alpha10)
-        rm -rf "${HOME}/.valet.sh"
-        # create tmp directory for cloning valet.sh
-        local tmp_dir
-        tmp_dir=$(mktemp -d) || error "Failed to create temporary directory, try running script at different path"
-        local src_dir
-        src_dir="$tmp_dir"
-        # clone project git repo to tmp dir
-        rm -rf "$tmp_dir" &> /dev/null
-        git clone --quiet "$APPLICATION_GIT_URL" "$tmp_dir"
-        # install
-        cp -r "${src_dir}" "${INSTALL_DIR}"
-        # get latest version
-        LATEST_VERSION=$(git_checkout_latest_version)
-        # remove tmp src dir
-        rm -rf "${src_dir}" &> /dev/null
-        # create PATH enhancement in relevant dotfiles
-        touch ~/.bash_profile
-        grep -q "${APPLICATION_EXPORT_PATH}" "${HOME}/.bash_profile" || echo "${APPLICATION_EXPORT_PATH}" >> "${HOME}/.bash_profile"
-        grep -q "${APPLICATION_EXPORT_PATH}" "${HOME}/.zshrc" || echo "${APPLICATION_EXPORT_PATH}" >> "${HOME}/.zshrc"
-        # execute PATH enhancement
-        # shellcheck disable=SC1090
-        source "${HOME}/.bash_profile"
-        # stop spinner
-        spinner_toggle
-        # output log
-        out success "Successfully installed version ${LATEST_VERSION}"
-    fi
 }
 
 ##############################################################################
@@ -305,41 +164,33 @@ function selfinstall() {
 ##############################################################################
 function upgrade() {
     spinner_toggle "Upgrading" -f
-    # get latest version
-    LATEST_VERSION=$(git_checkout_latest_version)
+    # fetch all tags from application git repo
+    git --git-dir="${APPLICATION_REPO_DIR}/.git" --work-tree="${APPLICATION_REPO_DIR}" fetch --tags --quiet
+    # get available release tags sorted by refname
+    GIT_TAGS=$(git --git-dir="${APPLICATION_REPO_DIR}/.git" --work-tree="${APPLICATION_REPO_DIR}" tag --sort "-v:refname" )
+    # get latest semver conform git version tag on current major version releases
+    for GIT_TAG in ${GIT_TAGS}; do
+        if [[ "${GIT_TAG}" =~ ^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(\-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$ ]]; then
+            git --git-dir="${APPLICATION_REPO_DIR}/.git" --work-tree="${APPLICATION_REPO_DIR}" checkout --force --quiet "${GIT_TAG}"
+            break
+        fi
+    done
     # stop spinner
     spinner_toggle
     # output specific upgrade status message
-    if [ "$(version_compare "${APPLICATION_VERSION}" "${LATEST_VERSION}")" = "0" ]; then
-        out success "Already on the latest version $LATEST_VERSION"
+    if [ "$(version_compare "${APPLICATION_VERSION}" "${GIT_TAG}")" = "0" ]; then
+        out success "Already on the latest version $GIT_TAG"
     else
-        out success "Successfully upgraded from ${APPLICATION_VERSION} to latest version ${LATEST_VERSION}"
+        out success "Successfully upgraded from ${APPLICATION_VERSION} to latest version ${GIT_TAG}"
     fi
-}
-
-##############################################################################
-# Fetches all tags from remote and checkouts latest semver compatible version
-##############################################################################
-function git_checkout_latest_version() {
-    # fetch all tags from application git repo
-    git --git-dir="${INSTALL_DIR}/.git" --work-tree="${INSTALL_DIR}" fetch --tags --quiet
-    # get available release tags sorted by refname
-    RELEASE_TAGS=$(git --git-dir="${INSTALL_DIR}/.git" --work-tree="${INSTALL_DIR}" tag --sort "-v:refname" )
-    # get latest semver conform git version tag on current major version releases
-    for GIT_TAG in $RELEASE_TAGS; do
-        if version_validate "${GIT_TAG}"; then
-            git --git-dir="${INSTALL_DIR}/.git" --work-tree="${INSTALL_DIR}" checkout --force --quiet "${GIT_TAG}"
-            echo "${GIT_TAG}"
-            return 0
-        fi
-    done
 }
 
 ##############################################################################
 # Prints the console tool header
 ##############################################################################
 function print_header() {
-    echo -e "\\033[1m$APPLICATION_NAME\\033[0m \\033[34m$APPLICATION_VERSION\\033[0m\\n"
+    echo -e "\\033[1m$APPLICATION_NAME\\033[0m \\033[34m$APPLICATION_VERSION\\033[0m"
+    printf "\\n"
 }
 
 ##############################################################################
@@ -359,8 +210,6 @@ function print_footer() {
         printf "\\e[34m"
         printf "\\n"
         printf "  Version: \\e[1m%s\\033[0m\\n" "$APPLICATION_VERSION"
-        printf "\\e[34m"
-        printf "  Application mode: \\e[1m%s\\033[0m\\n" "$APPLICATION_MODE"
         printf "\\e[34m"
         printf "  Execution time: \\e[1m%f sec.\\033[0m\\n" "$APPLICATION_EXECUTION_TIME"
         printf "\\e[34m"
@@ -443,7 +292,6 @@ function execute_ansible_playbook() {
 {
     "cli": {
         "name": "${APPLICATION_NAME}",
-        "mode": "${APPLICATION_MODE}",
         "version": "${APPLICATION_VERSION}",
         "args": [${parsed_args}]
     }
@@ -553,8 +401,8 @@ function process_args() {
 # Main
 ##############################################################################
 function main() {
-    print_header
     prepare
+    print_header
     process_args "$@"
     print_footer
     shutdown
